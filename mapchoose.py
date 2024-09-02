ff2eec93150ed450c4ea7cbeea1726242402bc92
Gtommo <tommymare@gmail.com>
2024-09-02 16:31:33 -0400

    added .gitignore and script

---
+++ mapchoose.py
@@ -0,0 +1,8 @@
+import random
+
+maps = ["Customs", "Ground Zero", "Shoreline", "Lighthouse", "Reserve", "Interchange", "Factory", "Woods", "Streets"]
+time = ["Day", "Night"]
+
+print(random.choice(maps))
+print(random.choice(time))
+
